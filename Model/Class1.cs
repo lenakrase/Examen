98869b7bccb329ab0c1c58789daae142b41c0311
egolodnikov <w8mepls23@gmail.com>
2016-01-30 18:54:16 +0700

    b

---
+++ Model/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Class1
+    {
+        public string Name;
+        public string Description;
+        public DateTime DueDate;
+
+        public enum TaskStatus
+        {
+            [Display(Name="Новая")]  New,
+            [Display(Name = "Выполняется")]Inprogress,
+            [Display(Name = "Отложена")]Postponded,
+            [Display(Name = "Завершена")]Completed,
+            [Display(Name="Отменена")]Canceled
+        }
+
+        public string Group;
+    }
+}
---
+++ ViewModel/EnumValuesExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using Model;
+
+namespace ViewModel
+{
+    public class EnumValuesExtension
+    {
+        
+    }
+}
